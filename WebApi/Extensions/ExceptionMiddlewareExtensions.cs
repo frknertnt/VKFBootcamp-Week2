69e336762f3ac8d7eaca52fdb6bfb07c46f9ffeb
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-18 00:28:58 +0300

    api configuration tier

---
+++ WebApi/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Entities.ErrorModel;
+using Entities.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Services.Contracts;
+using System.Net;
+
+namespace WebApi.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication app, ILoggerService logger)//middleware e istek geldiğinde ilginin bu metotdan geçmesini sağladık
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>//İsteği context olarak düşünelim
+                {
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if( contextFeature is not null )
+                    {
+                        context.Response.StatusCode = contextFeature.Error switch
+                        {
+                            NotFoundException => StatusCodes.Status404NotFound, //case
+                            _ => StatusCodes.Status500InternalServerError //case
+                        };
+
+                        logger.LogError($"Somenthing went wrong: {contextFeature.Error}");
+
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using NLog;
+using Repositories.EFCore;
+using Services.Contracts;
+using WebApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
+
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
+    .AddNewtonsoftJson();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();//Ýçerdiði parametre(IServiceCollection) dizi içerdiði için buraya yazmayabilirim
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureLoggerService();
+builder.Services.AddAutoMapper(typeof(Program));
+
+var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILoggerService>();
+app.ConfigureExceptionHandler(logger);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
