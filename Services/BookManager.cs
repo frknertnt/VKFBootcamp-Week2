b236c4c451ef63136efae5a5fad6edde879ce9e6
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-18 00:28:31 +0300

    service manager tier

---
+++ Services/BookManager.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Exceptions;
+using Entities.Models;
+using Repositories.Contracts;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BookManager : IBookService
+    {
+        private readonly IRepositoryManager _manager;
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+
+        public BookManager(IRepositoryManager manager, ILoggerService logger, IMapper mapper)
+        {
+            _manager = manager;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public Book CreateOneBook(Book book)
+        {
+            _manager.Book.CreateOneBook(book);
+            _manager.Save();
+            _logger.LogInfo("A book was created");
+            return book;
+        }
+
+        public void DeleteOneBook(int id, bool trackChanges)
+        {
+            //check entity
+            var entity = _manager.Book.GetOneBookById(id, trackChanges);
+            if (entity is null)
+                throw new BookNotFoundException(id);
+
+            _manager.Book.DeleteOneBook(entity);
+            _manager.Save();
+
+        }
+
+        public IEnumerable<Book> GetAllBooks(bool trackChanges)
+        {
+            return _manager.Book.GetAllBooks(trackChanges);
+        }
+
+        public Book GetOneBookById(int id, bool trackChanges)
+        {
+            var book = _manager.Book.GetOneBookById(id, trackChanges);
+            if (book is null)
+                throw new BookNotFoundException(id);
+
+            return book;
+        }
+
+        // İlişkisel veri tabanında kitapla beraber yazarın Id sini alarak kayıt işlemi veya güncelleme yapmak istenildiğinde Dto yardımcı olur
+        public void UpdateOneBook(int id, BookDtoForUpdate bookDto, bool trackChanges)
+        {
+            var entity = _manager.Book.GetOneBookById(id, trackChanges);
+            if (entity is null)
+                throw new BookNotFoundException(id);
+
+            // Mapping
+            //entity.Title = book.Title;  
+            //entity.Price = book.Price;
+
+            entity = _mapper.Map<Book>(bookDto);// yukarıdaki ifadeleri tek tek yazmaya gerek kalmadı
+
+            _manager.Book.Update(entity);
+            _manager.Save();
+        }
+    }
+}
