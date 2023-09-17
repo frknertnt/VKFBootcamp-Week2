2bfa39f4fbaec02c26536247ee45be98906f2629
Furkan Ertantu <furkanertantu@outlook.com>
2023-09-18 00:26:06 +0300

    model tier

---
+++ Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    /*DTO Özellikleri
+     *readonly (değeri değişmemelidir)
+     *immutable
+     *LINQ (üzerinde sorgular yazılabilir
+     *Ref type (classla aynı özellikleri taşır)
+     *Ctor gibi DTO tanımlama fırsatı verir
+     */
+
+
+    //public record BookDtoForUpdate
+    //{
+    //    public int Id { get; init; }//init ile immutable,readonly özelliği kazandırdık
+    //    public String Title { get; init; }//tanımlandığı yerde initialize edildiğinde değeri de verilir (set edilir)
+    //    public decimal Price { get; init; }
+    //}
+
+    public record BookDtoForUpdate(int Id, String Title, decimal Price);// yukarıdaki yazım tarzına alternatif bir yazım
+}
---
+++ Entities/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Entities.Exceptions
+{
+    public abstract class NotFoundException : Exception
+    {
+        protected NotFoundException(string message) : base(message) //abstract class olduğu için newlenemez o yüzden protected
+        {
+
+        }
+    }
+}
